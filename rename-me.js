815e9e0dd5226d4773f8daf2596723237bb4c7fa
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-15 13:51:10 -0400

    Making changes in the Object with the Properties & and added the components of JSON (Warehouse of Laptops & Desktop)

--- rename-me.js
+++ rename-me.js
@@ -19,13 +19,42 @@
 		var numOfPro = true;//Number of products
 
 //JSON data
-var monInven = {
-	""
+var monInven = {//Monitoring Inventory Weekly on Average
+	"Warehouse": {
+		"Laptops": {
+			"Employee's 1": {//Employee's working on model1 & information of products only
+				"Number of employee's": 30,//Number of employee's working in this section
+				"MN1 Light Received": 400,//Number of model1 shipments received in warehouse 
+				"MN1 Light Missing": 15//Never passes this number(Can't)
+			},
+			"Employee's 2": {//Employee's working on model2 & information of products only
+				"Number of employee's": 20,//Number of employee's working in this section
+				"MN2 Extreme Received": 350,//Number of model2 shipments received in warehouse
+				"MN2 Extreme Missing" 12//Never passes this number(Can't)
+			},	 
+		"Desktop":{
+			"Employee's 1": {//Employee's working on model1 & information of products only
+				"Number of employee's": 30,//Number of employee's working in this section
+				"LD Sheet Received": 400,//Number of model1 shipments received in warehouse 
+				"LD Sheet Missing": 15//Never passes this number(Can't)
+			},
+			"Employee's 2": {//Employee's working on model2 & information of products only
+				"Number of employee's": 20,//Number of employee's working in this section
+				"DLD Sheet Received": 350,//Number of model2 shipments received in warehouse
+				"DLD Sheet Missing" 12//Never passes this number(Can't)
+		}	
+			
+		},
+		
+		
+
+	}
 }
 
 
 
 //Object Properties of the three products
+//Consist of Property Number and String 	
  	var	objPro = {	
 		 Laptops: {
 			model: {
