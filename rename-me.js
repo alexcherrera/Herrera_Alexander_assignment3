bd763939c880eed2121ee2d98be74e227ae0205b
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-15 00:15:08 -0400

    Started by creating global variables

---
+++ rename-me.js
@@ -0,0 +1,10 @@
+//Global Variaibles: 
+	//Strings
+		var ceo = "Tim Roberts";
+		var manager = "Suzy Miller";
+		var compyName = "Tech Distribution";//Company name
+	//Array
+		var threePro = ["Laptops", "Desktops", "Tablets"];//Three products
+	//Boolean
+		var numOfPro = true;//Number of products
+
