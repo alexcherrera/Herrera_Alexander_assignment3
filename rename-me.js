cad7d8791a46b3a768fc325d756a444095b54f35
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-15 14:19:56 -0400

    Erase an extra global varaible

--- rename-me.js
+++ rename-me.js
@@ -8,15 +8,7 @@
 	//Boolean
 		var numOfPro = true;//Number of products
 
-//Global Variaibles: 
-	//Strings
-		var ceo = "Tim Roberts";
-		var manager = "Suzy Miller";
-		var compyName = "Tech Distribution";//Company name
-	//Array
-		var threePro = ["Laptops", "Desktops", "Tablets"];//Three products
-	//Boolean
-		var numOfPro = true;//Number of products
+
 
 //JSON data
 var monInven = {//Monitoring Inventory Weekly on Average
