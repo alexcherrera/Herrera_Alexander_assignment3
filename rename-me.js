58b96302437fbd00292135196c1765cede28fb91
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-15 00:20:48 -0400

    Added the Object Properties and started working on the JSON

--- rename-me.js
+++ rename-me.js
@@ -8,3 +8,64 @@
 	//Boolean
 		var numOfPro = true;//Number of products
 
+//Global Variaibles: 
+	//Strings
+		var ceo = "Tim Roberts";
+		var manager = "Suzy Miller";
+		var compyName = "Tech Distribution";//Company name
+	//Array
+		var threePro = ["Laptops", "Desktops", "Tablets"];//Three products
+	//Boolean
+		var numOfPro = true;//Number of products
+
+//JSON data
+var monInven = {
+	""
+}
+
+
+
+//Object Properties of the three products
+ 	var	objPro = {	
+		 Laptops: {
+			model: {
+				name: "MN1 Light",
+				price: 900,
+				size: "13 inches"
+			},	
+		 	model2: {
+		 		name: "MN2 Extreme",
+				price: 1500,
+				size: "15 inches"
+		 	},
+		 },
+		 Desktops: {
+		 	model1: {
+				name: "LD Sheet",
+				price: 650,
+				size: "17 inches"
+			},	
+		 	model2: {
+		 		name: "DLD Quad",
+				price: 1300,
+				size: "21 inches"
+		    },
+		 },
+		 Tablets: {
+		 	model1: {
+				name: "D-Screen",
+				price: 350,
+				size: "8 inches"
+			},	
+		 	model2: {
+		 		name: "DX Quad Screen",
+				price: 650,
+				size: "11 inches"
+		    },
+		 },   
+	};
+
+
+console.log(threePro[0]);
+console.log(objPro.Desktops.model2.name);
+console.log("hello");
