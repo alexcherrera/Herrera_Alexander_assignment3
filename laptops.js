813816999c9f0557b1ecb7c6de51523b874e76e4
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-16 17:36:46 -0400

    creating functions & calculating models of Laptops

--- laptops.js
+++ laptops.js
@@ -3,6 +3,7 @@
 		var ceo = "Tim Roberts";
 		var manager = "Suzy Miller";
 		var compyName = "Tech Distribution";//(Company name)
+		var wk = "weekly";//Used throughout for weekly averages
 	//Array
 		var threePro = ["Laptops ", " Desktops", " Tablets"];//(Three products)
 	//Number
@@ -10,74 +11,95 @@
 	//Boolean
 		var numOfPro = true;//(Number of Products)Two different types of models for each products
 
-//Sold Products but separated by different objects
-	//Property Array 
-		var wk = "weekly";
-		var	lapSol = {
+//Sold Products on average "weekly" but separated by different objects
+	//Object (Property Array)
+		var	lapSol = {//Laptops Sold per model
 			sold: [
 				525,//Model 1 
 				375//Model 2
 			]
 		};
-		var	lapRet = {
+			//Variables for sold Laptop Models that store arrays in objects(positions)
+				var src = "sold";
+				var mod1So = lapSol.sold[0];//Model 1 sold
+				var mod2So = lapSol.sold[1];//Model 2 sold	
+					console.log("On average based by " + wk + " inventory checks " + threePro[0] + src + " are estimated at " + mod1So);	
+	//Object (Property Array)
+		var	lapRet = {//Laptops returned
 			returned: [
 				15,//Model 1
 				13//Model 2
 			]
 		};
-var src = "sold";
-	console.log("On average based by " + wk + " inventory checks " + threePro[0] + src + " are estimated at " + lapSol.sold[0]);
+			//Variables for sold Laptop Models that store arrays in objects(positions)
+				var ret = "returned";
+				var mod1re = lapRet.returned[0];//Model 1 returned
+				var mod2re = lapRet.returned[1];//Model 2 returned
+//Calculate Required Amount of weekly Laptops
+	var calReqLap = function (amount1, amount2) {
+		var totBoLapRe = amount1 + amount2;//Total amount for both laptop models "received"
+		var reqMod1 = 500;//Requirement amount of Laptop model 1
+		var reqMod2 = 350;//Requirement amount of Laptop model 2
+		var exMod1 = mod1So - reqMod1;//Extra amount of Laptop model 1 
+		var totBoLaRe = reqMod1 + reqMod2;//Total Both Laptop Models Required 
+			if (reqMod1 < amount1) {
+				console.log("Their is always an extra amount of " + exMod1 + " " + threePro[0] + "ordered.");
+				return;
+			}
+		return exMod1;
+	};
+	calReqLap(mod1So, mod2So);
 
 
 
-
-	
-
-
-//Employee's
-//JSON data
+//JSON data employees
 var numEmpLa = {//(Number of Employees Laptops)
-	"emplMo1&2": [//(Employees Models 1 & 2)
+	"emplMod": [//(Employees Models 1 & 2)
 		{
 			"lapMN1Em" : 60,//(Laptop MN1-Light Employees)
+		},
+		{
 			"lapMN2Em" : 25//(Laptop MN2-Extreme Employees)
 		}
 	]
 };
 
+//JSON Data stored in values
+	var callEm1 = numEmpLa.emplMod[0].lapMN1Em;
+	var callEm2 = numEmpLa.emplMod[1].lapMN2Em;
+
+	var emplMo1 = function (num1,num2) {
+			var totEmBoth = num1 + num2; 
+				//if ()
+
+			console.log(" " + num1 + " " + num2);
+				return totEmBoth;
+				};
+			emplMo1(callEm1,callEm2);
+//Laptops Model 1 MN1-Light
+//var lapMod1inf = function() {
+
+//};
+//lapMod1inf();
 //Each object has Properties of the three products
 	//Consist of Properties (Number and String) 	
  		//Object of Laptops
- 			var	lapMod1 = {	
-					name: "MN1-Light",
-					price: 900,
-					size: "13 inches",
+ 			var	lapMod1 = {//Laptop Model 1	
+				name: "MN1-Light",
+				price: 900,
+				size: "13 inches",
 			};
-		 	var lapMod2 = {
-		 			name: "MN2-Extreme",
-					price: 1500,
-					size: "15 inches",
-		 			send: function() {
+		 	var lapMod2 = {//Laptop Model 2
+	 			name: "MN2-Extreme",
+				price: 1500,
+				size: "15 inches",
+		 	};
+			var lapBoMod = {//Laptop Both Models
+				send: function() {
 						prMod1 = lapMod1.price;
 						prMod2 = lapMod2.price;
 						console.log(prMod1 + prMod2);
 		 			}
-		 	};
-			lapMod2.send();
+			};
+			lapBoMod.send();
 
-/*var calOneLap = function(lapInd) {
-	var storSolLapMo = solLap.models[lapInd];
-	var storSolLapSold = solLap.sold[lapInd];
-	var storSolLapRet = solLap.returned[lapInd];
-	console.log(storSolLapMo);
-	console.log("hey");
-}
-
-
-var calAllLap = function () {
-	for (var lapInd = 0; lapInd < solLap.models.Length; lapInd++) {
-		var fLapInd = solLap.models[lapInd];
-		calOneLap(lapInd);
-	}
-};
-calAllLap();*/ 
