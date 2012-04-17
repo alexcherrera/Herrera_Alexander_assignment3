167e3f321cab32ebb60265c1e0d05d9bdec13a71
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-17 10:05:36 -0400

    Created a JSON file & organzing

---
+++ JSON.js
@@ -0,0 +1,45 @@
+//Laptop Model 1 & 2
+	//All infomation
+		var lapBoMd = {//Both Laptop Models
+			"model1": {
+				"name": "MN1-Light",
+				"price": 900,
+				"size": "13 inches",
+				"compact": true,
+			},
+			"mode2": {
+				"name": "MN2-Extreme",
+				"price": 1500,
+				"size": "15 inches",
+				"compact": false,
+			}
+		};
+		var desBoMd = {//Both Desktop Models
+			"model1": {
+				"name": "LDSheet",
+				"price": 650,
+				"size": "17 inches",
+				"printer": true,//If it comes with a printer
+			},
+			"mode2": {
+				"name": "DLD-Quad",
+				"price": 1300,
+				"size": "21 inches",
+				"printer": false//If it comes with a printer
+			}
+		};
+		var tabBoMd = {//Both Tablet Models
+			"model1": {
+				"name": "D-Screen",
+				"price" : 350,
+				"size": "8 inches"
+				"case": false,//If a case is included
+			},	
+		 	"model2": {
+		 		"name": "DX-Quad-Screen",
+				"price": 650,
+				"size": "11 inches"
+		    	"case": true,//If a case is included
+		    }
+		};
+		
--- laptops.js
+++ laptops.js
@@ -6,11 +6,19 @@
 		var wk = "weekly";//Used throughout for weekly averages
 	//Array
 		var threePro = ["Laptops ", " Desktops", " Tablets"];//(Three products)
+	//Array's for Laptops model 1 & 2 received & missing
+		var receLaMod = [600, 450]//received shipment Laptops Models
+		var missLaMod = [15, 12]//missing Laptops Models
 	//Number
 		var quaOfEaMod = 2;//Quantity of each model
 	//Boolean
 		var numOfPro = true;//(Number of Products)Two different types of models for each products
 
+//Setting values based on models 1 & 2 for received & missing
+var reLaMd1 = receLaMod[0];//Received Laptop Model 1
+var reLaMd2 = receLaMod[1];//Received Laptop Model 2
+var misLaMd1 = missLaMod[0];
+var misLaMd2 = missLaMod[1];
 //Sold Products on average "weekly" but separated by different objects
 	//Object (Property Array)
 		var	lapSol = {//Laptops Sold per model
@@ -35,6 +43,7 @@
 				var ret = "returned";
 				var mod1re = lapRet.returned[0];//Model 1 returned
 				var mod2re = lapRet.returned[1];//Model 2 returned 
+
 
 
 
@@ -78,11 +87,13 @@ var numEmpLa = {//(Number of Employees Laptops)
 				price: 900,
 				size: "13 inches",
 			};
+			var naLapMod1 = lapMod1.name;//Laptop Model 1 Name 
 		 	var lapMod2 = {//Laptop Model 2
 	 			name: "MN2-Extreme",
 				price: 1500,
 				size: "15 inches",
 		 	};
+		 	var naLapMod2 = lapMod2.name;//Laptop Model 2 Name 
 			var lapBoMod = {//Laptop Both Models
 				send: function() {
 						prMod1 = lapMod1.price;
@@ -91,21 +102,32 @@ var numEmpLa = {//(Number of Employees Laptops)
 		 			}
 			};
 			lapBoMod.send();
-var naLapMod1 = lapMod1.name;
-
+	
 //Calculate Required Amount of weekly Laptops for Model 1
 	var calReqLap = function ([amount1, amount2]) {
-		var reqMod1 = 500;//Requirement amount of Laptop model 1
-		var reqMod2 = 350;//Requirement amount of Laptop model 2
-		var totBoLaRe = reqMod1 + reqMod2;//Total Both Laptop Models Required 
-		var exMod1 = mod1So - reqMod1;//Extra amount of Laptop model 1 	
-			if (reqMod1 < amount1) {
-				console.log("Their should always be an extra amount of " + exMod1 + " " + naLapMod1 + " " + threePro[0] + "ordered, which is required.");
-				} else {
-					console.log("This may result in a shortage of " + threePro[0])
+		var exMod1 = reLaMd1 - mod1So;//Extra amount of Laptop model 1 	
+			if (amount1 < reLaMd1) {
+				if (misLaMd1 < exMod1) {
+					console.log("Their should always be an extra amount of " + exMod1 + " " + naLapMod1 + " " + threePro[0] + "ordered, which is required.");
 				}
-			
+				else {
+					console.log("This may result in a shortage of " + threePro[0]);
+				}
+			}
+		var exMod2 = reLaMd2 - mod2So;//Extra amount of Laptop model 2
+				if (amount2 < reLaMd2) {
+					if (misLaMd2 < exMod2) {
+						console.log("Also their should always be an extra amount of " + exMod2 + " " + naLapMod2 + " " + threePro[0] + "ordered.");
+					}
+					else {
+						console.log("This may result in a shortage of " + threePro[0]);
+					}
+				}
+				
 		return [exMod1,exMod2];
 	};
-	var retCalReqLap = calReqLap([mod1So,mod2so]);
-
+	var retCalReqLap = calReqLap([mod1So,mod2So]);
+//Calculate Required Amount of weekly Laptops for Model 2
+/*	var 
+	var exMod2 = reLaMd2 - mod2So;//Extra amount of Laptop model 2
+*/
